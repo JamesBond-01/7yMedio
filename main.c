47f2bf1828a57a7e049bbdbee9a9584e6c0aa05a
Leandro Gomez <leandrodanielgomez@gmail.com>
2021-08-07 20:58:49 -0300

    Modificaciones main.c

--- main.c
+++ main.c
@@ -3,14 +3,15 @@
 
 int main () {
 
-    //Monto con el que empieza la banca.
+    // Monto con el que empieza la banca.
     int banksWallet = 100000;
+    int cards, points, players, rounds;
+    int *cardStack[40];
 
     printf ("\n=================================");
     printf ("\nBienvenido al juego Siete y Medio");
     printf ("\n=================================\n");
 
-    int players, rounds;
     printf ("\nPara comenzar, por favor ingrese los siguientes datos:\n");
 
     printf ("\nCantidad de jugadores con los que desee jugar [1 - 5]: ");
@@ -21,11 +22,13 @@ int main () {
         scanf ("%d", &players);
     }
 
-    int playersWallets[players];
+    // Billetera donde se almacena el dinero de cada jugador.
+    int playersWallets[players];  
+    // Array que contiene la apuesta que hace cada jugador por ronda.
     int playersBet[players];
     size_t i, j;
 
-    //Le asignamos a cada jugador su monto inicial.
+    //Le asignamos a cada jugador su monto inicial ($5000).
     for (i = 0; i <= players - 1; i++) {
         playersWallets[i] = 5000;
     }
@@ -39,6 +42,10 @@ int main () {
     }
 
     for (i = 1; i <= rounds; i++) {
+    // Llamamos a la funcion getCardsStack() para obtener el mazo de 40 cartas, mezcladas aleatoriamente
+    // por cada ronda.
+    *getCardsStack();
+
         for (j = 0; j <= players - 1; j++) {
             printf ("\nJugador %d, ingrese el monto que desee apostar [$150 - $1500]: ", (j+1));
             scanf ("%d", &playersBet[j]);
@@ -47,8 +54,9 @@ int main () {
                 printf ("\nPor favor, ingrese un valor correcto [$150 - $1500]: ");
                 scanf ("%d", &playersBet[j]);  
             }
-
             playersWallets[j] = playersWallets[j] - playersBet[j];
+
+            popCard(*cardStack);
         }
 
     }
