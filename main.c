22efb13dca9e405ac33db3a49d9fd767e245fb4c
Leandro Gomez <leandrodanielgomez@gmail.com>
2021-08-06 21:44:55 -0300

    Modificaciones main.c

--- main.c
+++ main.c
@@ -26,6 +26,37 @@ int main () {
         scanf ("%d", &rounds);
     }
 
+    //Array que contiene la plata que tiene cada jugador.
+    int bankWallet = 100000;
+    int playersBet[players];
+    int playersWallets[players];
+    size_t i, j;
+
+    for (i = 0; i <= players - 1; i++) {
+        playersWallets[i] = 5000;
+        //printf ("\nPlayer %d = %d", (i+1) , playersWallets[i]);
+    }
+
+    for (i = 1; i <= rounds; i++) {
+        for (j = 0; j <= players - 1; j++) {
+
+            printf ("\nJugador %d, ingrese el monto que desee apostar [$150 - $1500]: ", (j+1));
+            scanf ("%d", &playersBet[j]);
+
+            while (playersBet [j] < 150 || playersBet[j] > 1500) {
+                printf ("\nPor favor, ingrese un valor correcto [$150 - $1500]: ");
+                scanf ("%d", &playersBet[j]);  
+            }
+
+            playersWallets[j] = playersWallets[j] - playersBet[j];
+        }
+
+        for (i = 0; i <= players - 1; i++) {
+            printf ("\nApuesta jugador %d: %d. Saldo = $%d", (i+1) , playersBet[i], playersWallets[i]);
+        }
+
+    }
+    
 
     return 0;
 }
